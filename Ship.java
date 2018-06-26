af2b43f23f54096a1d42215892fc3c4236274c2c
darDomanski <dardomanski@gmail.com>
2018-06-26 11:37:12 +0200

    initial commit

---
+++ Ship.java
@@ -0,0 +1,27 @@
+public class Ship {
+    Square[] ship;
+    int length;
+    String arrangment;
+    char vertCoordinate;
+    int horCoordinate;
+    
+
+    public Ship(int length, String arrangment) {
+        this.ship = new Square[length];
+        this.length = length;
+        this.arrangment = arrangment;
+    }
+    
+    public createShip(char vertCoordinate, int horCoordinate, int length) {
+
+    } 
+
+    public boolean check
+
+    public putShipInTheOcean(Ocean ocean) {
+
+    }
+
+
+
+}
