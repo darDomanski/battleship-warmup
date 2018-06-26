3ddb38193a729a2c50ce4e592649e0332c279c80
darDomanski <dardomanski@gmail.com>
2018-06-26 14:53:12 +0200

    reorganized Ship logic

--- Ship.java
+++ Ship.java
@@ -1,22 +1,31 @@
+import java.util.ArrayList;
+
 public class Ship {
-    Square[] ship;
+    ArrayList<Square> shipSquares;
     int length;
     String arrangment;
     char vertCoordinate;
     int horCoordinate;
+
+    public static void main(String[] args) {
+        System.out.p;
+    }
     
 
-    public Ship(int length, String arrangment) {
-        this.ship = new Square[length];
+    public Ship(int length, String arrangment, ArrayList<Square> squaresOfShip) {
+        this.shipSquares = squaresOfShip;
         this.length = length;
         this.arrangment = arrangment;
     }
     
-    public createShip(String arrangment, char vertCoordinate, int horCoordinate, int length) {
+    public Ship createShip(String arrangment, char vertCoordinate, int horCoordinate, int length) {
         if (checkCoordinates(ocean, arrangment, vertCoordinate, horCoordinate, length)) {
+            Ship ship = new Ship(length, arrangment);
             
+            for (Square sqr: ship.shipSquares) {
+                ocean.
+            }
         }
-
     } 
 
 
