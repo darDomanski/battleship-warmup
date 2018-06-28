e3d6157c3c1944ea17dbad96d135b07d569085ec
Karol Trzaska <karolantonitrzaska@gmail.com>
2018-06-28 14:51:14 +0200

    Added function getChar to Common

--- Common.java
+++ Common.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Common {
     public static int getInt() {
         String intString;
@@ -21,6 +23,25 @@ public class Common {
     public static String getInput() {
         Scanner stringInput = new Scanner(System.in);
         String text = stringInput.nextLine();
+        return text;
+    }
+
+    public static char getChar()
+    {
+        Scanner charInput = new Scanner(System.in);
+        
+        while (isNotInteger) {
+
+            char text = charInput.next().charAt(0);
+
+            try {
+                number = Integer.parseInt(intString);
+                isNotInteger = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Error! Type a number...");
+                isNotInteger = true;
+            }
+        }
         return text;
     }
 }
